0b15cf943732a01c761a08bf865a0017577ab9d5
NISAN05 <sanoranigtherson@yahoo.com>
2023-05-02 11:04:00 -0700

    configuration splashscreen

--- app/src/main/java/com/example/tedactu/MainActivity.java
+++ app/src/main/java/com/example/tedactu/MainActivity.java
@@ -1,14 +1,45 @@
 package com.example.tedactu;
 
 import androidx.appcompat.app.AppCompatActivity;
+import com.codepath.asynchttpclient.AsyncHttpClient;
+import com.codepath.asynchttpclient.RequestParams;
+import com.codepath.asynchttpclient.callback.TextHttpResponseHandler;
 
 import android.os.Bundle;
+import android.widget.Button;
+
+import okhttp3.Headers;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button bt1;
+    public static final String BASE_URL = "https://tedactu.com";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        bt1 = findViewById(R.id.bt1);
+
+
+        AsyncHttpClient client = new AsyncHttpClient();
+        RequestParams params = new RequestParams();
+        params.put("limit", "5");
+        params.put("page", "0");
+        client.get("https://api.thecatapi.com/v1/images/search", params, new TextHttpResponseHandler() {
+                    @Override
+                    public void onSuccess(int statusCode, Headers headers, String response) {
+                        // called when response HTTP status is "200 OK"
+                    }
+
+                    @Override
+                    public void onFailure(int statusCode, Headers headers, String errorResponse, Throwable t) {
+                        // called when response HTTP status is "4XX" (eg. 401, 403, 404)
+                    }
+                }
+        );
+
+
+
+
+
     }
 }
