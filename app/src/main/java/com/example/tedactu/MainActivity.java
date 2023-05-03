8c90cdee81f339ff0cde722b758baf1d9cacd00c
Joaking95 <Fredericbekend@gmail.com>
2023-05-03 12:38:41 -0700

    read data from the api

--- app/src/main/java/com/example/tedactu/MainActivity.java
+++ app/src/main/java/com/example/tedactu/MainActivity.java
@@ -3,16 +3,19 @@ package com.example.tedactu;
 import androidx.appcompat.app.AppCompatActivity;
 import com.codepath.asynchttpclient.AsyncHttpClient;
 import com.codepath.asynchttpclient.RequestParams;
+import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler;
 import com.codepath.asynchttpclient.callback.TextHttpResponseHandler;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 
 import okhttp3.Headers;
 
 public class MainActivity extends AppCompatActivity {
     Button bt1;
-    public static final String BASE_URL = "https://tedactu.com";
+    public static final String BASE_URL = "https://tedactu.com/wp-json/wp/v2/categories?page=1&per_page=10&search=e";
+    public  static final String TAG = "MAinActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,22 +23,21 @@ public class MainActivity extends AppCompatActivity {
         bt1 = findViewById(R.id.bt1);
 
 
+       //Send a request ton the endpoint wp
         AsyncHttpClient client = new AsyncHttpClient();
-        RequestParams params = new RequestParams();
-        params.put("limit", "5");
-        params.put("page", "0");
-        client.get("https://api.thecatapi.com/v1/images/search", params, new TextHttpResponseHandler() {
-                    @Override
-                    public void onSuccess(int statusCode, Headers headers, String response) {
-                        // called when response HTTP status is "200 OK"
-                    }
 
-                    @Override
-                    public void onFailure(int statusCode, Headers headers, String errorResponse, Throwable t) {
-                        // called when response HTTP status is "4XX" (eg. 401, 403, 404)
-                    }
-                }
-        );
+        client.get(BASE_URL, new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Headers headers, JSON json) {
+               Log.d(TAG,"SUCCESS" ) ;
+
+            }
+
+            @Override
+            public void onFailure(int statusCode, Headers headers, String response, Throwable throwable) {
+                Log.e(TAG,"Faillure" ) ;
+            }
+        });
 
 
 
---
+++ app/src/main/java/com/example/tedactu/models/CategoriesModel.java
@@ -0,0 +1,17 @@
+package com.example.tedactu.models;
+
+public class CategoriesModel {
+
+    int id;
+    int counter;
+    String description;
+    String link;
+    String name;
+    String slug;
+    String taxonomy;
+    int parent;
+    Object meta;
+
+
+
+}
